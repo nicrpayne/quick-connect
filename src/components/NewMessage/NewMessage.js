0eb9bfa30170fef665cbe867e039536ab651f6bd
Nic Payne <nic.payne@wnins.com>
2021-06-28 14:28:11 -0500

    adding changes

--- src/components/NewMessage/NewMessage.js
+++ src/components/NewMessage/NewMessage.js
@@ -149,10 +149,10 @@ class NewMessage extends Component {
               <br></br>
               Your Message:
               <p>
-                "{this.getGreeting()}
-                {this.getGuestNameById(this.state.newMessage.guestId)}, and
+                "{this.getGreeting()}{' '}
+                {this.getGuestNameById(this.state.newMessage.guestId)} and
                 welcome to{this.getCompanyById(this.state.newMessage.companyId)}
-                {this.state.newMessage.companyId}! Room number
+                ! Room number {' '}
                 {this.getRoomNumberByGuestId(this.state.newMessage.guestId)} is
                 now ready you. Enjoy your stay, and let us know if you need
                 anything."
