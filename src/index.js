8396e224fd71e64dd58e6643a317730aed39afd4
Nic Payne <nic.payne@wnins.com>
2021-06-28 23:06:56 -0500

    adding changes to index/app/routes for styling

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
+
 import rootReducer from './redux/reducers/_root.reducer'; // imports ./redux/reducers/index.js
 import rootSaga from './redux/sagas/_root.saga'; // imports ./redux/sagas/index.js
 import "@fortawesome/fontawesome-free/css/all.min.css";
